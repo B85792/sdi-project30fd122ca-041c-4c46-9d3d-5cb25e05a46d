1b78c38b0fd4869fa046fd57b7bf86e0a5822b90
harold arnett <lynn72@fullsail.edu.com>
2013-02-20 22:24:50 -0500

    1 down

--- arnett_harold_assignment3.js
+++ arnett_harold_assignment3.js
@@ -5,4 +5,47 @@
 // SDI 1302
 // Sam's PET SHOP
 
+// My Global Variables
 
+var myName = "Sam";
+var storeName = "Sam's Pet Shop";
+var numberOfRooms = 5; 
+
+// My Procedure
+var myName = true;
+
+ if ( myName === true) { 
+    console.log("Sam is opening a new pet store in town!");
+    } else {
+    console.log("Sam will not be opening a new pet store in town!");
+};
+
+var storeOwner = {
+    name : "Sam",
+	age : 40,
+	interests: [
+	 		"Saltwater fish"
+	], 		
+};
+
+console.log(storeOwner.name + " loves " + storeOwner.interests);
+
+
+
+
+
+
+
+var storeName = "Sam's Pet Shop";
+	var room1 = "Fish Room";
+	var numberOfTanks = 12;
+	var numberOfSaltwaterTanks = 6;
+	var numberOfFreshwaterTanks = 6;
+			
+	if (numberOfSaltwaterTanks < numberOfFreshwaterTanks) {
+		console.log("That's not enough tanks of fish!");   		   
+  		} else {
+  		console.log("I will have more than enough fish!");
+  };
+     
+ 
