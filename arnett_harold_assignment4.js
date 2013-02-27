6d55b6f12caf02e7d6fc2ca35c57d2e52a5c2ac2
harold arnett <lynn72@fullsail.edu.com>
2013-02-27 12:46:13 -0500

    test

---
+++ arnett_harold_assignment4.js
@@ -0,0 +1,16 @@
+// alert("JavaScript works!");
+
+// Harold Arnett
+// Project 4
+// SDI 1302
+// FUNCTION LIBRARIES
+
+var myName = "Sam";
+var storeName = "Sam's Pet Shop";
+var numberOfRooms = 4; 
+var myName = "Sam";
+var storeName = "Sam's Pet Shop";
+var numberOfRooms = 4; 
+var myName = "Sam";
+var storeName = "Sam's Pet Shop";
+var numberOfRooms = 4; 
