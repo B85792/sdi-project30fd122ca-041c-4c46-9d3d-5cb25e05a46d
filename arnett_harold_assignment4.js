3c63a40a3c4bb1281c8ca30ceb0c564dff0925c4
harold arnett <lynn72@fullsail.edu.com>
2013-02-27 12:47:56 -0500

    test

--- arnett_harold_assignment4.js
+++ arnett_harold_assignment4.js
@@ -5,12 +5,4 @@
 // SDI 1302
 // FUNCTION LIBRARIES
 
-var myName = "Sam";
-var storeName = "Sam's Pet Shop";
-var numberOfRooms = 4; 
-var myName = "Sam";
-var storeName = "Sam's Pet Shop";
-var numberOfRooms = 4; 
-var myName = "Sam";
-var storeName = "Sam's Pet Shop";
-var numberOfRooms = 4; 
+
